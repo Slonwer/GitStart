cab1f6873b9f90e63d7038fd32589d7e0305d51f
Blareovski1 <gabriel.nascimento287@etec.sp.gov.br>
2024-04-02 21:20:57 -0300

    remover

--- crud/js/funcoes.js
+++
@@ -1,6 +0,0 @@
-function confirmacaoExclusaoUser(id){
-    var resposta = confirm("Deseja remover esse registro?");
-    if (resposta == true) {
-		  window.location.href = "delete.php?id=" + id;
-    }
-}
